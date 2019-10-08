497daa4b4839c780e0d929d83b831d1da84bdb23
StoneT2000 <stoneztao@gmail.com>
2019-10-08 16:25:53 -0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+
+// TODO: Add secret to prevent 3rd party posts not from github
 router.post("/", (req, res) => {
   // console.log("POST TO PAYLOAD HEADERS: ",req.headers);
 
@@ -22,7 +24,7 @@ router.post("/", (req, res) => {
  * Triggered when a pull request is assigned, unassigned, labeled, unlabeled, opened, edited,
  * closed, reopened, synchronize, ready_for_review, locked, unlocked or
  * when a pull request review is requested or removed.
- * @param req - the request object
+ * @param req - the request object. Note that the actual data is in req.body
  */
 function pullRequestEvent(req) {
   console.log("Initator of Pull Request: " + req.body.pull_request.user.login);
@@ -32,11 +34,12 @@ function pullRequestEvent(req) {
  *
  */
 function pushEvent(req) {
+  console.log(`SHA of latest commit at ${req.body.commits[0].timestamp}: ${req.body.commits[0].id}`);
   console.log("Who Pushed: " + req.body.pusher.name);
 }
 
 function deploymentEvent(req) {
-  console.log("Deployment created at: " + req.data.body.deployment.created_at);
+  console.log("Deployment created at: " + req.body.deployment.created_at);
 }
 
 module.exports = router;
